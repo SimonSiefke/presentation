8bbd9d7351c545dfdb767ab583466f47ecb84d0b
SimonSiefke <simon.siefke@gmail.com>
2017-06-19 20:36:33 +0200

    Create example3.js

---
+++ example3.js
@@ -0,0 +1,4 @@
+
+ document.getElementById('runExample3').addEventListener('click', () => {
+     document.getElementById('example3').innerHTML += " a = 0&nbsp&nbsp&nbsp&nbspb = 3&nbsp&nbsp&nbsp&nbspc = 0.8&nbsp&nbsp&nbsp&nbspb * c = 2.4"
+ })
