54bc613d22b8658200c2b34b17d80557490e71d6
SimonSiefke <simon.siefke@gmail.com>
2017-06-21 21:39:50 +0200

    Update example6.js

--- example6.js
+++ example6.js
@@ -1,5 +1,5 @@
 
       document.getElementById('runExample6').addEventListener('click', () => {
-     document.getElementById('example6').innerHTML += " warning: reference to local variable ‘result’ returned [-Wreturn-local-addr]"
+     document.getElementById('example6').innerHTML += " 1"
  })
 //ok
