60c2a6c1b8a889a91f49fe5075b71e878feb1572
SimonSiefke <simon.siefke@gmail.com>
2017-06-21 21:46:11 +0200

    Update all.js

--- all.js
+++ all.js
@@ -1,1 +1,20 @@
 
+ document.getElementById('runExample1').addEventListener('click', () => {
+     document.getElementById('example1').innerHTML += " a = 2"
+ })
+ document.getElementById('runExample2').addEventListener('click', () => {
+     document.getElementById('example2').innerHTML += " World, Hello"
+ })
+      document.getElementById('runExample4').addEventListener('click', () => {
+     document.getElementById('example4').innerHTML += " warning: reference to local variable ‘result’ returned [-Wreturn-local-addr]"
+ })
+document.getElementById('runExample5').addEventListener('click', () => {
+     document.getElementById('example5').innerHTML += " Process exited with code: 0"
+ })
+
+      document.getElementById('runExample6').addEventListener('click', () => {
+     document.getElementById('example6').innerHTML += " 1"
+ })
+document.getElementById('runExample7').addEventListener('click', () => {
+     document.getElementById('example7').innerHTML += " 1.2"
+ })
